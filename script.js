3d7e88552035b5fbed0c01e5138d3293f0072588
Neon <105565447+BK-06@users.noreply.github.com>
2023-01-24 08:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+'use strict';
+
+const h3El = document.getElementById('wordCount');
+
+function myFunction(e){
+    let text = document.getElementById('evaluatedText').value;
+    h3El.textContent = `Word Count : ${text.length}`;
+}
